86f6935d38135bc46ea6ba9ad0a264925981e6a5
Hien Tran <tranthanhhien5@gmail.com>
2023-05-20 19:27:25 +0700

    chore: Update login component

---
+++ src/components/Login.js
@@ -0,0 +1,93 @@
+import { connect } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+import { handleLogin } from "../actions/authedUser";
+
+const Login = ({ dispatch, loggedIn }) => {
+  const [username, setUsername] = useState("sarahedo");
+  const [password, setPassword] = useState("password123");
+
+  if (loggedIn) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirectUrl = urlParams.get("redirectTo");
+    return <Navigate to={redirectUrl ? redirectUrl : "/"} />;
+  }
+
+  const handleUsername = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+  };
+
+  const handlePassword = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(handleLogin(username, password));
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex justify-center">
+      <img src="https://t3.ftcdn.net/jpg/02/90/83/56/360_F_290835671_jFAkOJOyejt4uZ0b7NvZtoNlD92L7cHh.jpg" />
+
+      <div className="flex flex-col mx-8 justify-center items-center">
+        <h1 className="text-3xl font-bold mt-9 mb-4" data-testid="login-heading">
+          Login
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium text-slate-700">
+              Username
+            </label>
+            <div className="mt-1">
+              <input
+                value={username}
+                onChange={handleUsername}
+                type="text"
+                name="username"
+                id="username"
+                data-testid="username"
+                className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
+              />
+            </div>
+          </div>
+          <div className="mt-6">
+            <label htmlFor="password" className="block text-sm font-medium text-slate-700">
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                value={password}
+                onChange={handlePassword}
+                type="password"
+                name="password"
+                id="password"
+                data-testid="password"
+                className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
+              />
+            </div>
+          </div>
+          <div className="mt-6 text-right">
+            <button
+              type="submit"
+              data-testid="submit"
+              className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ authedUser }) => ({
+  loggedIn: !!authedUser,
+});
+
+export default connect(mapStateToProps)(Login);
