4df72f6bb981e591f8ae25547cc0a909995c500b
Hien Tran <tranthanhhien5@gmail.com>
2023-05-20 19:26:53 +0700

    chore: Update navbar

---
+++ src/components/NavBar.test.js
@@ -0,0 +1,38 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import NavBar from "./NaBar";
+import { setAuthedUser } from "../actions/authedUser";
+
+describe("NavBar", () => {
+  it("should render the component", () => {
+    store.dispatch(setAuthedUser({ id: "sarahedo", password: "" }));
+
+    const component = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(component).toBeDefined();
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should display username of logged in user", () => {
+    store.dispatch(setAuthedUser({ id: "sarahedo", password: "" }));
+
+    const component = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const userSpanElement = component.getByTestId("user-information");
+    expect(userSpanElement.textContent).toBe("User: sarahedo");
+  });
+});
